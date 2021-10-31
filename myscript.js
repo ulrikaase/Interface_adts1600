f0e79a856852dab7b3af810be92db264de94438f
ulrikaase <89570152+ulrikaase@users.noreply.github.com>
2021-10-31 22:30:20 +0100

    bilder og shizz

--- myscript.js
+++ myscript.js
@@ -17,3 +17,46 @@ function myFunction() {
       }
     }
   }
+
+  var slideIndex = 1;
+  showSlides(slideIndex);
+  
+  // Next/previous controls
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+  
+  // Thumbnail image controls
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+  
+  function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+  }
+
+  //Image changer:
+
+  function changeImage() {
+
+    if (document.getElementById("imgClickAndChange").src == "Images/lightOff.png") 
+    {
+        document.getElementById("imgClickAndChange").src = "Images\lamp-logo-light-bulb-technology-icons-3.png";
+    }
+    else 
+    {
+        document.getElementById("imgClickAndChange").src = "Images/lightOff.png";
+    }
+}
